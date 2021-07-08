7d369062e8e14ba93da2e1a0f59f281ce5602617
Peppe <simogabri1999@gmail.com>
2021-07-08 02:30:59 +0200

    Fixed minor bug in cell update

--- Assets/Scripts/CellController.cs
+++ Assets/Scripts/CellController.cs
@@ -11,6 +11,7 @@ public class CellController : MonoBehaviour
     private int value;
 
     private bool lockedValue;
+    private bool valid;
 
     private InputField inputField;
 
@@ -20,6 +21,7 @@ public class CellController : MonoBehaviour
         row = int.Parse(name.Substring(0, 1)); //The second letter in the name of the gameobjects indicates the row
         column = int.Parse(name.Substring(1, 1)); //The second letter in the name of the gameobjects indicates the column
         value = 0;
+        valid = true;
 
         lockedValue = false;
         inputField = GetComponent<InputField>();
@@ -35,33 +37,46 @@ public class CellController : MonoBehaviour
         return column;
     }
 
+    private void resetCell()
+    {
+        value = 0;
+        inputField.text = "";
+        inputField.GetComponent<Image>().color = Color.white;
+    }
+
     public void updateCell()
     {
         if (lockedValue)
             return;
 
         if (string.IsNullOrEmpty(inputField.text))
-        {
-            value = 0;
-            inputField.GetComponent<Image>().color = Color.white;
-        }
+            resetCell();
         else
         {
             value = int.Parse(inputField.text);
-
             //The view updates the model only if the cell is valid
-            if (LocalUtilities.getGameManager().validateCell(row, column, value))
-            {
-                LocalUtilities.getGameManager().updateCell(row, column, value);
-                inputField.GetComponent<Image>().color = Color.white;
-
-                checkGameOver();
-            }
-            else
+            if (!LocalUtilities.getGameManager().validateCell(row, column, value))
             {
                 inputField.GetComponent<Image>().color = inputField.GetComponent<Image>().color = new Color(253f / 255f, 170f / 255f, 170f / 255f);
+                valid = false;
                 //Notify User
             }
+        }
+    }
+
+    public void confirmUpdate()
+    {
+        if (lockedValue)
+            return;
+
+        if (!valid)
+            resetCell();
+        else
+        {
+            LocalUtilities.getGameManager().updateCell(row, column, value);
+            inputField.GetComponent<Image>().color = Color.white;
+            checkGameOver();
+            valid = true;
         }
     }
 
